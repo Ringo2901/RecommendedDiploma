7fe5acb5e17738274fe87a81cf4f26b6a7843665
Ringo2901 <nekitale@gmail.com>
2025-02-19 15:44:06 +0300

    Initial commit

---
+++ src/main/java/by/bsuir/aleksandrov/recommendeddiploma/RecommendedDiplomaApplication.java
@@ -0,0 +1,13 @@
+package by.bsuir.aleksandrov.recommendeddiploma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommendedDiplomaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecommendedDiplomaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/bsuir/aleksandrov/recommendeddiploma/RecommendedDiplomaApplicationTests.java
@@ -0,0 +1,13 @@
+package by.bsuir.aleksandrov.recommendeddiploma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendedDiplomaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
